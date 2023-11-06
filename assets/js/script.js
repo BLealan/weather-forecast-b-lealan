8133027599ba5491b186824342c6d385b34c6ace
BLealan <143086275+BLealan@users.noreply.github.com>
2023-11-06 16:45:09 +0000

    added save city search to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
+var searchForCity = document.querySelector("#city-search");
+var searchButton = document.querySelector("#searchBtn");
+
 var createWeatherCard = document.getElementById("weather-cards");
 
-function getApi(){
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+searchButton.addEventListener("click", function(event){
+    event.preventDefault();
 
-    fetch(requestUrl)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
-}
+    var savedCity = searchForCity.value;
 
-getApi();
+    localStorage.setItem("city", savedCity);
+
+});
+
+// function getApi(){
+//     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+
+//     fetch(requestUrl)
+//     .then(function (response){
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     })
+// }
+
+// getApi();
