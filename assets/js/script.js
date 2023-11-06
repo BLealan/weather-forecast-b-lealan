e219dcae07a5a044e276432987026cb0e27cf968
BLealan <143086275+BLealan@users.noreply.github.com>
2023-11-06 16:59:25 +0000

    added function to make long/lat from city

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,32 @@ var searchButton = document.querySelector("#searchBtn");
 
 var createWeatherCard = document.getElementById("weather-cards");
 
+//Function to convert city in local storage to co-ordinates
+
+function cityToCoordinates(){
+
+    var city = localStorage.getItem("city");
+
+    var fetchCityCoord = `http://api.openweathermap.org/geo/1.0/direct?q={${city}}&appid={5a2bf608390639e1f4cb6c9ed307b7e6}`;
+
+    fetch(fetchCityCoord)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    })
+};
+
+//Click event to save value of search bar to local storage
 searchButton.addEventListener("click", function(event){
     event.preventDefault();
 
     var savedCity = searchForCity.value;
 
     localStorage.setItem("city", savedCity);
+
+    cityToCoordinates();
 
 });
 
