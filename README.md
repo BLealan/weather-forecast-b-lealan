92fa263de9427904e351fbd3d4d75e073988c17b
BLealan <143086275+BLealan@users.noreply.github.com>
2023-11-09 12:15:55 +0000

    added icon display

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Since the OpenWeather API needs co-ordinates (latitude and longitude) these first needed to be gathered. However, after trying to use the OpenWeather geolocation and being unable to return anything, I instead used an API from API Ninjas (found in sources below). This only needed a text input, gathered from the local storage saved city, to return the data, the co-ordinates from which were saved to variables.
 
 - https://api-ninjas.com/api/geocoding
+- https://stackoverflow.com/questions/7342957/how-do-you-round-to-one-decimal-place-in-javascript
+- https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var longitude = 0;
 var savedCity = searchForCity.value;
 var createWeatherCard = document.getElementById("weather-cards");
 
+
 function getApi(latitude, longitude){
     var APIKey = "5a2bf608390639e1f4cb6c9ed307b7e6";
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKey}`;
@@ -17,7 +18,7 @@ function getApi(latitude, longitude){
         var saveWeather = data;
         var savedCity = searchForCity.value;
         localStorage.setItem(savedCity,JSON.stringify(saveWeather));
-        console.log(saveWeather);
+        // console.log(saveWeather);
     })
 }
 
@@ -36,6 +37,7 @@ function cityToCoordinates(){
             console.error('Error: ', jqXHR.responseText);
         }
     })
+    
 };
 
 //Function to generate weather cards
@@ -46,28 +48,48 @@ function generateCards(){
         var date = $(`<p class="date[i]">Date:${cardDate}</p>`);
         var weatherCardEl = $("#weather-cards");
         var singleCardEl = $(`<div class="single-card[i] card-style"></div>`)
-        var icon = $(`<img class="icon[i]"/>`);
+        var iconEl = $(`<img class="weather-icon" src="" alt="Weather Icon"/>`);
         var temperature = $(`<p class="temperature[i]">Temperature:</p>`);
         var windSpeed = $(`<p class="wind-speed[i]">Wind Speed:</p>`);
         var humidity = $(`<p class="humidity[i]">Humidity:</p>`);
 
         weatherCardEl.append(singleCardEl);
-        singleCardEl.append(date, icon, temperature, windSpeed, humidity);
+        singleCardEl.append(date, iconEl, temperature, windSpeed, humidity);
     }
 };
 
 //Click event to save value of search bar to local storage an create history button
 searchButton.addEventListener("click", function(event){
-
+    
+    var savedCity = searchForCity.value;
     var historyList = $("#search-history");
-    var historyButtonEl = $(`<button class ="${savedCity}" class="btn saveBtn" aria-label="save">${savedCity}</button>`);
+    var historyButtonEl = $(`<button class="${savedCity} saveBtn" aria-label="save">${savedCity}</button>`);
 
     historyList.append(historyButtonEl);
 
     cityToCoordinates();
 
-    generateCards();
+    if (searchForCity.value){
+    generateCards()};
+
+    var testResult = JSON.parse(localStorage.getItem(savedCity));
+    var iconEl = $(".weather-icon");
+    var weatherIcon = testResult.weather[0].icon;
+    var iconEl = `http://openweathermap.org/img/w/${weatherIcon}.png`;
+    $(".weather-icon").attr("src", iconEl);
+    var temperature = testResult.main.temp;
+    var windSpeed = testResult.wind.speed;
+    var humidity = testResult.main.humidity;
+    //Default temp from API is in Kelvin so needs to be converted
+    temperature -= 273.15;
+    var roundedTemperature = Math.round(temperature *10)/10;
+    // console.log(weatherIcon, roundedTemperature, windSpeed, humidity);
 });
 
-var testResult = JSON.parse(localStorage.getItem("New York"));
-console.log(testResult);
+// var saveButton = $(".saveBtn");
+// saveButton.addEventListener("click", function(){
+//     var $thisButton = $(this);
+//     var buttonText = $thisButton(".saveBtn").val();
+//     console.log(buttonText);
+// });
+
